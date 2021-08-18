7c04cc335d5be5448347575a376909e788109ccd
Ghai <Donna.Ghai@wolterskluwer.com>
2021-08-18 15:22:10 +0530

    commit message

---
+++ src/main/java/com/example/jpa/Jpademo/JpaDemoApplication.java
@@ -0,0 +1,39 @@
+package com.example.jpa.Jpademo;
+
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class JpaDemoApplication implements CommandLineRunner{
+
+	private Logger logger=LoggerFactory.getLogger(this.getClass());
+	@Autowired
+	PersonJpaRepo dao;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDemoApplication.class, args);
+	}
+//in jdbc you have to define the table into the H2 databse but in jpa it is not requires
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		logger.info("all users->{}",dao.findAll());
+	logger.info("user id 1001->{}",dao.findById(1001));
+	//logger.info("user id 1002->{}",dao.deleteById(1002));
+		dao.deleteById(1002);
+//		//System.out.print(dao.findAll());
+	logger.info("inserting 1009->{}",dao.insert(new Person(1010,"tara","berlin",new Date())));
+	logger.info("update 1003->{}",dao.update(new Person(1003,"pari","berlin",new Date())));
+//	
+		
+	}
+
+}
---
+++ src/main/java/com/example/jpa/Jpademo/PersonJpaRepo.java
@@ -0,0 +1,54 @@
+package com.example.jpa.Jpademo;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+
+//repository
+//Transcation management//insert update delete it becomes import
+// whenever there are 3-4 steps or updates transcations are important
+//either all fail or all successful
+@Repository
+@Transactional
+public class PersonJpaRepo {
+	//all operations performed into a particular session are stored inside your entity manager
+	//all operations are stored in persistence context which is the interface of entity manager
+	@PersistenceContext
+	EntityManager entityManager;
+	
+	//connect to database
+	//JPQL
+	public List<Person> findAll(){
+		TypedQuery<Person> namedQuery=entityManager.createNamedQuery("find_all_persons",Person.class);
+		return namedQuery.getResultList();
+		
+		
+		
+	}
+	public Person findById(int id) {
+		return entityManager.find(Person.class, id);
+		
+	}
+
+	// if you want update or insert you have to use entityManager.merge
+	public Person update(Person person) {
+		return entityManager.merge(person);
+		
+	}
+	public Person insert(Person person) {
+		return entityManager.merge(person);
+		
+	}
+	public void deleteById(int id) {
+		Person person=findById(id);
+		 entityManager.remove(person);
+		
+	}
+	
+}
