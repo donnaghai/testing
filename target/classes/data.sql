be448af9eaef349dac88c631cd29c8e13e2ec0e8
donna.ghai <donnaghai18@gmail.com>
2021-08-19 11:01:45 +0530

    donna

---
+++ target/classes/data.sql
@@ -0,0 +1,20 @@
+--create table person
+--(
+--id integer not null,
+--name varchar(255) not null,
+--location varchar(255),
+--birth_date timestamp,
+--primary key(id)
+--);
+INSERT INTO PERSON
+(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(1001,'RANGA','DELHI',sysdate());
+INSERT INTO PERSON
+(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(1002,'donna','DELHI',sysdate());
+INSERT INTO PERSON
+(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(1003,'RANGA','DELHI',sysdate());
+INSERT INTO PERSON
+(ID,NAME,LOCATION,BIRTH_DATE)
+VALUES(1004,'RANGA','DELHI',sysdate());
