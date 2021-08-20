2c45ab958a5608a4dbdc66d11d1d4b3cc4234c68
donna.ghai <donnaghai18@gmail.com>
2021-08-20 11:41:50 +0530

    new23

--- src/main/java/com/example/jpa/Jpademo/JpaDemoApplication.java
+++ src/main/java/com/example/jpa/Jpademo/JpaDemoApplication.java
@@ -27,10 +27,12 @@ public class JpaDemoApplication implements CommandLineRunner{
 		// TODO Auto-generated method stub
 		logger.info("all users->{}",dao.findAll());
 			logger.info("all users->{}",dao.findAll());
+			hiuoyhioufois
 				logger.info("inserting 1009->{}",dao.insert(new Person(1010,"tara","berlin",new Date())));
 	logger.info("inserting 1009->{}",dao.insert(new Person(1010,"tara","berlin",new Date())));
+	sxuytsudga
 	logger.info("update 1003->{}",dao.update(new Person(1003,"pari","berlin",new Date())));
-
+gsiuhdilahskjjfhkdsfd
 	logger.info("user id 1001->{}",dao.findById(1001));
 	logger.info("user id 1001->{}",dao.findById(1001));
 	//logger.info("user id 1002->{}",dao.deleteById(1002));
@@ -38,7 +40,9 @@ public class JpaDemoApplication implements CommandLineRunner{
 //		//System.out.print(dao.findAll());
 	logger.info("inserting 1009->{}",dao.insert(new Person(1010,"tara","berlin",new Date())));
 	logger.info("inserting 1009->{}",dao.insert(new Person(1010,"tara","berlin",new Date())));
+	gulsaaiudhiskhln.,ml.kpoid
 	logger.info("update 1003->{}",dao.update(new Person(1003,"pari","berlin",new Date())));
+	shagjsagudsakujjhdkshckdscd
 //	
 		
 	}
